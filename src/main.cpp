099466dc7b9201eefa062a7a65119cd2adced926
zhangjunyi <58250587+junyiha@users.noreply.github.com>
2024-08-25 12:18:45 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,45 @@
  * @copyright Copyright (c) 2024
  * 
  */
+// STL
 #include <iostream>
 #include <algorithm>
+#include <string>
+
+// opencv
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
-#include "QVector"
+
+// Qt
+#include "QtWidgets/QWidget"
+#include "QtWidgets/QApplication"
+#include "QPushButton"
 
 int main(int argc, char* argv[])
 {
     std::cerr << "Hello world!\n";
+    if (argc <=1)
+    {
+        std::clog << "Invalid program argument!!!\n";
+        return -1;
+    }
+
+    std::string path{argv[1]};
     
-    cv::Mat img = cv::imread("C:/Users/anony/Desktop/1.jpg");
+    cv::Mat img = cv::imread(path);
     cv::imshow("image", img);
     std::cerr << img.cols << ", " << img.rows << "\n";
     cv::waitKey(100);
 
     QVector<int> arr{1, 2, 3};
-    std::for_each(arr.begin(), arr.end(), [](int num){std::cerr << num << ", ";});
+    std::for_each(arr.begin(), arr.end(), [](int num){std::cerr << num << "\n";});
     
-    
-    return 0;
+    QApplication app(argc, argv);
+    QWidget w;
+    w.show();
+    auto obj = new QPushButton;
+    std::clog << "QPushButton className(): " << obj->metaObject()->className() << "\n";
+
+    return app.exec();
+    // return 0;
 }
