0828c562d9b8d39434d4f32b81644a2b9b92aaa2
zhangjunyi <58250587+junyiha@users.noreply.github.com>
2024-12-31 15:07:18 +0800

    边线检测业务以过程式风格实现

--- src/onnxruntime.cpp
+++ src/onnxruntime.cpp
@@ -68,95 +68,226 @@ std::vector<std::vector<cv::Point2f>> GetPossibleLines(const cv::Mat& mask)
     return possible_lines;
 }
 
-void LoadModel()
+struct Context_t
 {
-    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "test");
-    Ort::SessionOptions session_options;
-    std::wstring model_path{ L"D:/Robot/models/pp_liteseg_stdc1_softmax_20241021.onnx" };
-    model_path = L"C:/Users/anony/Documents/GitHub/cpp-win/data/ppseg_model_wb_20241120.onnx";
-    Ort::Session session(env, model_path.c_str(), session_options);
-
-    std::size_t num_input_nodes = session.GetInputCount();
-    std::size_t num_output_nodes = session.GetOutputCount();
-
-    Ort::AllocatorWithDefaultOptions allocator;
-    std::string input_name = session.GetInputName(0, allocator);
-    std::string output_name = session.GetOutputName(0, allocator);
-
-    auto input_info = session.GetInputTypeInfo(0);
-    auto input_shape = input_info.GetTensorTypeAndShapeInfo().GetShape();
-    auto input_dims = input_shape[0];
-
+    Ort::Env* env;
+    Ort::Session* session;
     std::vector<char*> input_names;
-    for (int i = 0; i < num_input_nodes; i++)
-    {
-        auto input_info = session.GetInputTypeInfo(i);
-        auto input_name = session.GetInputName(i, allocator);
-        input_names.push_back(input_name);
-        auto input_shape = input_info.GetTensorTypeAndShapeInfo().GetShape();
-        auto input_dims = input_shape[0];
-        std::cerr << "input name: " << input_name << "\n";
-        std::cerr << "input shape(BCHW): ";
-        for (auto dim : input_shape)
-        {
-            std::cerr << dim << ", ";
-        }
-        std::cerr << "\n";
-    }
-
     std::vector<char*> output_names;
-    for (int i = 0; i < num_output_nodes; i++)
+    std::vector<int64_t> input_shape;
+    std::vector<int64_t> output_shape;
+};
+
+int global_height{ 0 };
+int global_width{ 0 };
+
+
+Context_t Init(const std::wstring& model_path)
+{
+    Ort::Env* env = new Ort::Env(ORT_LOGGING_LEVEL_WARNING, "test");
+    Ort::SessionOptions session_options;
+    Ort::Session* session = new Ort::Session(*env, model_path.c_str(), session_options);
+
+    Context_t context;
+    context.env = env;
+    context.session = session;
+    context.input_shape = session->GetInputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+    context.output_shape = session->GetOutputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+    Ort::AllocatorWithDefaultOptions allocator;
+    context.input_names.push_back(session->GetInputName(0, allocator));
+    context.output_names.push_back(session->GetOutputName(0, allocator));
+
+    return context;
+}
+
+void Preprocess(const cv::Mat& img, const std::vector<int64_t>& input_shape, cv::Mat& rgb_img, cv::Mat& gray_img, cv::Mat& blob)
+{
+    if (img.channels() != 3)
     {
-        auto output_info = session.GetOutputTypeInfo(i);
-        auto output_name = session.GetOutputName(i, allocator);
-        output_names.push_back(output_name);
-        auto output_shape = output_info.GetTensorTypeAndShapeInfo().GetShape();
-        auto output_dims = output_shape[0];
-        std::cerr << "output name: " << output_name << "\n";
-        std::cerr << "output shape(BCHW): ";
-        for (auto dim : output_shape)
-        {
-            std::cerr << dim << ", ";
-        }
-        std::cerr << "\n";
+        cv::cvtColor(img, rgb_img, cv::COLOR_GRAY2BGR);
+        gray_img = img.clone();
+    }
+    else
+    {
+        rgb_img = img.clone();
+        cv::cvtColor(img, gray_img, cv::COLOR_BGR2GRAY);
     }
 
-    std::string image_path{ "C:/Users/anony/Documents/GitHub/cpp-win/data/LineCam_6_2024-12-23-15-59-55.png" };
-    // std::string image_path{ "C:/Users/anony/Documents/GitHub/cpp-win/data/LineCam_3_2024-12-23-15-59-54.png" };
+    global_height = input_shape.at(2);
+    global_width = input_shape.at(3);
+    cv::resize(rgb_img, rgb_img, cv::Size(global_width, global_height));
+    cv::resize(gray_img, gray_img, cv::Size(global_width, global_height));
+    rgb_img.convertTo(rgb_img, CV_32F);
+    rgb_img = ((rgb_img / 255.0f) - 0.5f) / 0.5f;
 
-    cv::Mat image = cv::imread(image_path);
-    if (image.empty())
-    {
-        std::cerr << "load image failed\n";
-        return;
-    }
-    cv::Mat image_gray = image.clone();
-    cv::resize(image_gray, image_gray, cv::Size(input_shape.at(2), input_shape.at(3)));
-    if (image.channels() != 3)
-    {
-        cv::cvtColor(image, image, cv::COLOR_GRAY2BGR);
-    }
-    cv::resize(image, image, cv::Size(input_shape.at(2), input_shape.at(3)));
-    image.convertTo(image, CV_32F);
-    image = ((image / 255.0f) - 0.5f) / 0.5f;
-    cv::Mat blob = cv::dnn::blobFromImage(image, 1.0, cv::Size(input_shape.at(2), input_shape.at(3)), cv::Scalar(0, 0, 0), true, false);
+    blob = cv::dnn::blobFromImage(rgb_img, 1.0, cv::Size(global_width, global_height), cv::Scalar(0, 0, 0), true, false);
+}
 
+bool Infer(cv::Mat blob, Context_t& ctx, std::vector<Ort::Value>& output_tensors)
+{
     std::vector<Ort::Value> input_tensors;
     auto allocator_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
-    input_tensors.emplace_back(Ort::Value::CreateTensor<float>(allocator_info, blob.ptr<float>(), blob.total(), input_shape.data(), input_shape.size()));
+    input_tensors.emplace_back(Ort::Value::CreateTensor<float>(allocator_info, blob.ptr<float>(), blob.total(), ctx.input_shape.data(), ctx.input_shape.size()));
 
-    std::vector<Ort::Value> output_tensors = session.Run(Ort::RunOptions{ nullptr }, &input_names.at(0), input_tensors.data(), input_tensors.size(), output_names.data(), output_names.size());
-    if (output_tensors.size() != num_output_nodes)
+    output_tensors = ctx.session->Run(Ort::RunOptions{ nullptr }, &ctx.input_names.at(0), input_tensors.data(), input_tensors.size(), ctx.output_names.data(), ctx.output_names.size());
+    if (output_tensors.size() != ctx.session->GetOutputCount())
     {
         std::cerr << "output tensors size not equal to num_output_nodes\n";
-        return;
+        return false;
     }
     if (!output_tensors.front().IsTensor())
     {
         std::cerr << "output tensor is not a tensor\n";
-        return;
+        return false;
     }
 
+    return true;
+}
+
+bool ProcessReferMask(cv::Mat referMask, cv::Mat image_gray, std::vector<float>& line_res)
+{
+    auto possible_lines = GetPossibleLines(referMask);
+
+    if (possible_lines.empty())
+    {
+        std::cerr << "possibleLines empty\n";
+        return false;
+    }
+
+    double max_dist = 0;
+    int max_index = 0;
+    for (int i = 0; i < possible_lines.size(); i++)
+    {
+        double dist_i = possible_lines.at(i).at(0).y > possible_lines.at(i).at(1).y ? possible_lines.at(i).at(0).y : possible_lines.at(i).at(1).y;
+
+        CaculateDistance(possible_lines.at(i).at(0), possible_lines.at(i).at(1));
+
+        if (dist_i < 768 * 0.15)
+        {
+            continue;
+        }
+
+        if (possible_lines.at(i).at(0).y < 768 * 0.5)
+        {
+            continue;
+        }
+
+        cv::Point2f p1((possible_lines.at(i).at(0).x + possible_lines.at(i).at(1).x) / 2, (possible_lines.at(i).at(0).y + possible_lines.at(i).at(1).y) / 2);
+        if (p1.x + 30 > global_width || p1.y + 30 > global_height)
+        {
+            continue;
+        }
+
+        cv::Rect rect(p1.x, p1.y, 30, 30);
+        cv::Mat roi = image_gray(rect);
+        double mean_color = cv::mean(roi)[0];
+        if (mean_color > 180)
+        {
+            continue;
+        }
+
+        if (dist_i > max_dist)
+        {
+            max_dist = dist_i;
+            max_index = i;
+        }
+    }
+
+    if (max_index == -1)
+    {
+        return false;
+    }
+
+    line_res = FitLine(possible_lines.at(max_index));
+
+    if (line_res.empty())
+    {
+        std::cerr << "line_res empty\n";
+        return false;
+    }
+
+    bool result = std::all_of(line_res.begin(), line_res.end() - 1, [](float val) { return val >= 0.0; });
+    if (!result)
+    {
+        std::cerr << "line_res invalid\n";
+        return false;
+    }
+
+    return true;
+}
+
+bool ProcessInkMask(cv::Mat inkMask, cv::Mat image_gray, const std::vector<float>& refer_line, std::vector<float>& line_res)
+{
+    auto possible_lines = GetPossibleLines(inkMask);
+    double dgree_ref = atan((refer_line.at(3) - refer_line.at(1)) / (refer_line.at(2) - refer_line.at(0))) * 180 / CV_PI;
+
+    double min_dist = 100000;
+    int min_index = -1;
+    double max_dist = 0;
+    int max_index = 0;
+    for (int i = 0; i < possible_lines.size(); i++)
+    {
+        double dist_i = CaculateDistance(possible_lines.at(i).at(0), possible_lines.at(i).at(1));
+        if (dist_i < 768 * 0.15)
+        {
+            continue;
+        }
+
+        double max_ref_y = refer_line.at(1) > refer_line.at(3) ? refer_line.at(1) : refer_line.at(3);
+        double max_ink_y = possible_lines.at(i).at(0).y < possible_lines.at(i).at(1).y ? possible_lines.at(i).at(1).y : possible_lines.at(i).at(0).y;
+        if (max_ink_y > max_ref_y)
+        {
+            continue;
+        }
+
+        double slope_i = (possible_lines.at(i).at(1).y - possible_lines.at(i).at(0).y) / (possible_lines.at(i).at(1).x - possible_lines.at(i).at(0).x);
+        double dgree_i = atan(slope_i) * 180 / CV_PI;
+        if (std::abs(dgree_i - dgree_ref) > 10)
+        {
+            continue;
+        }
+
+        if (dist_i > max_dist)
+        {
+            max_dist = dist_i;
+            max_index = i;
+        }
+
+        double ref_ink_dist_y = std::abs(max_ink_y - max_ref_y);
+        if (min_dist > ref_ink_dist_y)
+        {
+            min_dist = ref_ink_dist_y;
+            min_index = i;
+        }
+    }
+
+    if (min_index == -1)
+    {
+        std::cerr << "min_index invalid\n";
+        return false;
+
+    }
+
+    line_res = FitLine(possible_lines.at(min_index));
+
+    if (line_res.empty())
+    {
+        std::cerr << "line_res empty\n";
+        return false;
+    }
+
+    bool result = std::all_of(line_res.begin(), line_res.end() - 1, [](float val) { return val >= 0.0; });
+    if (!result)
+    {
+        std::cerr << "line_res invalid\n";
+        return false;
+    }
+
+    return true;
+}
+
+bool Postprocess(std::vector<Ort::Value>& output_tensors, cv::Mat image_gray, std::vector<float>& refer_line, std::vector<float>& ink_line)
+{
     auto output_shape = output_tensors.front().GetTensorTypeAndShapeInfo().GetShape();
     std::vector<int> mask_sz(output_shape.begin(), output_shape.end());
     cv::Mat res(mask_sz, CV_32F, output_tensors.front().GetTensorMutableData<float>());
@@ -189,7 +320,7 @@ void LoadModel()
     if (mask_sum < 100)
     {
         std::cerr << "mask sum less than 100\n";
-        return;
+        return false;
     }
 
     cv::Mat inkMask = cv::Mat::zeros(argmax_image.size(), CV_8UC1);
@@ -208,145 +339,60 @@ void LoadModel()
             }
         }
     }
-    auto possible_lines = GetPossibleLines(referMask);
-
-    if (possible_lines.empty())
+    bool result{ false };
+    result = ProcessReferMask(referMask, image_gray, refer_line);
+    if (!result)
     {
-        std::cerr << "possibleLines empty\n";
-        return;
-    }
-    double max_dist = 0;
-    int max_index = 0;
-    for (int i = 0; i < possible_lines.size(); i++)
-    {
-        double dist_i = possible_lines.at(i).at(0).y > possible_lines.at(i).at(1).y ? possible_lines.at(i).at(0).y : possible_lines.at(i).at(1).y;
-
-        CaculateDistance(possible_lines.at(i).at(0), possible_lines.at(i).at(1));
-
-        if (dist_i < 768 * 0.15)
-        {
-            continue;
-        }
-
-        if (possible_lines.at(i).at(0).y < 768 * 0.5)
-        {
-            continue;
-        }
-
-        cv::Point2f p1((possible_lines.at(i).at(0).x + possible_lines.at(i).at(1).x) / 2, (possible_lines.at(i).at(0).y + possible_lines.at(i).at(1).y) / 2);
-        if (p1.x + 30 > image_gray.cols || p1.y + 30 > image_gray.rows)
-        {
-            continue;
-        }
-
-        cv::Rect rect(p1.x, p1.y, 30, 30);
-        cv::Mat roi = image_gray(rect);
-        double mean_color = cv::mean(roi)[0];
-        if (mean_color > 180)
-        {
-            continue;
-        }
-
-        if (dist_i > max_dist)
-        {
-            max_dist = dist_i;
-            max_index = i;
-        }
+        std::cerr << "ProcessReferMask failed\n";
+        return false;
     }
 
-    std::vector<float> line_res;
-    if (max_index != -1)
+    result = ProcessInkMask(inkMask, image_gray, refer_line, ink_line);
+    if (!result)
     {
-        line_res = FitLine(possible_lines.at(max_index));
+        std::cerr << "ProcessInkMask failed\n";
+        return false;
     }
 
-    if (line_res.at(0) >= 0.0 && line_res.at(1) >= 0.0 && line_res.at(2) >= 0.0 && line_res.at(3) >= 0.0)
-    {
+    return true;
+}
 
-    }
-    else
+void LoadModel()
+{
+    std::wstring model_path{ L"D:/Robot/models/pp_liteseg_stdc1_softmax_20241021.onnx" };
+    model_path = L"C:/Users/anony/Documents/GitHub/cpp-win/data/ppseg_model_wb_20241120.onnx";
+
+    Context_t ctx = Init(model_path);
+
+    std::string image_path{ "C:/Users/anony/Documents/GitHub/cpp-win/data/LineCam_6_2024-12-23-15-59-55.png" };
+    // std::string image_path{ "C:/Users/anony/Documents/GitHub/cpp-win/data/LineCam_3_2024-12-23-15-59-54.png" };
+
+    cv::Mat image = cv::imread(image_path);
+    if (image.empty())
     {
-        std::cerr << "line_res invalid\n";
+        std::cerr << "load image failed\n";
         return;
     }
 
-    if (line_res.empty())
+    cv::Mat image_gray, image_rgb, blob;
+    Preprocess(image, ctx.input_shape, image_rgb, image_gray, blob);
+    image = image_rgb;
+
+    std::vector<Ort::Value> output_tensors;
+    Infer(blob, ctx, output_tensors);
+
+    std::vector<float> ink_line;
+    std::vector<float> refer_line;
+    bool result = Postprocess(output_tensors, image_gray, refer_line, ink_line);
+    if (!result)
     {
-        std::cerr << "line_res empty\n";
-        return;
-    }
-    cv::line(image, cv::Point(line_res.at(0), line_res.at(1)), cv::Point(line_res.at(2), line_res.at(3)), cv::Scalar(0, 0, 255), 2);
-
-    possible_lines = GetPossibleLines(inkMask);
-    double dgree_ref = atan((line_res.at(3) - line_res.at(1)) / (line_res.at(2) - line_res.at(0))) * 180 / CV_PI;
-
-    double min_dist = 100000;
-    int min_index = -1;
-    for (int i = 0; i < possible_lines.size(); i++)
-    {
-        double dist_i = CaculateDistance(possible_lines.at(i).at(0), possible_lines.at(i).at(1));
-        if (dist_i < 768 * 0.15)
-        {
-            continue;
-        }
-
-        double max_ref_y = line_res.at(1) > line_res.at(3) ? line_res.at(1) : line_res.at(3);
-        double max_ink_y = possible_lines.at(i).at(0).y < possible_lines.at(i).at(1).y ? possible_lines.at(i).at(1).y : possible_lines.at(i).at(0).y;
-        if (max_ink_y > max_ref_y)
-        {
-            continue;
-        }
-
-        double slope_i = (possible_lines.at(i).at(1).y - possible_lines.at(i).at(0).y) / (possible_lines.at(i).at(1).x - possible_lines.at(i).at(0).x);
-        double dgree_i = atan(slope_i) * 180 / CV_PI;
-        if (std::abs(dgree_i - dgree_ref) > 10)
-        {
-            continue;
-        }
-
-        if (dist_i > max_dist)
-        {
-            max_dist = dist_i;
-            max_index = i;
-        }
-
-        double ref_ink_dist_y = std::abs(max_ink_y - max_ref_y);
-        if (min_dist > ref_ink_dist_y)
-        {
-            min_dist = ref_ink_dist_y;
-            min_index = i;
-        }
-    }
-
-    if (min_index != -1)
-    {
-        line_res = FitLine(possible_lines.at(min_index));
-    }
-    else
-    {
-        std::cerr << "min_index invalid\n";
-        return;
-    }
-    std::vector<float> inkLine;
-    if (line_res.at(0) >= 0.0 && line_res.at(1) >= 0.0 && line_res.at(2) >= 0.0 && line_res.at(3) >= 0.0)
-    {
-        inkLine = line_res;
-    }
-    else
-    {
-        std::cerr << "line_res invalid\n";
+        std::cerr << "Postprocess failed\n";
         return;
     }
 
-    if (inkLine.empty())
-    {
-        std::cerr << "inkLine empty\n";
-        return;
-    }
-    cv::line(image, cv::Point(inkLine.at(0), inkLine.at(1)), cv::Point(inkLine.at(2), inkLine.at(3)), cv::Scalar(0, 255, 0), 2);
+    cv::line(image, cv::Point(refer_line.at(0), refer_line.at(1)), cv::Point(refer_line.at(2), refer_line.at(3)), cv::Scalar(0, 0, 255), 2);
+    cv::line(image, cv::Point(ink_line.at(0), ink_line.at(1)), cv::Point(ink_line.at(2), ink_line.at(3)), cv::Scalar(0, 255, 0), 2);
 
-    cv::imshow("inkMask", inkMask);
-    cv::imshow("referMask", referMask);
     cv::imshow("image", image);
     cv::waitKey(0);
 }
